31999c773e2b0af8bf632c3b9fd77df72db7ad2f
Jamie Zhang <akjamie.zhang@gmail.com>
2019-04-04 10:49:52 +0800

    test tls

--- src/main/java/org/akj/springboot/vault/utils/HikariDatasourceConfig.java
+++ src/main/java/org/akj/springboot/vault/utils/HikariDatasourceConfig.java
@@ -1,37 +1,37 @@
-package org.akj.springboot.vault.utils;
-
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
-@Configuration
-@ConfigurationProperties(prefix = "spring.datasource")
-public class HikariDatasourceConfig extends HikariConfig {
-	
-	@Value("${spring.datasource.username}")
-	private String userName;
-	
-	@Value("${spring.datasource.password}")
-	private String pwd;
-
-	@Bean
-	@Primary
-	public DataSource dataSource() throws SQLException {
-		
-		// use the credential configured in Vault to replace the spring config
-		this.setUsername(this.userName);
-		this.setPassword(this.pwd);
-		
-		return new HikariDataSource(this);
-	}
-
-}
+//package org.akj.springboot.vault.utils;
+//
+//import java.sql.SQLException;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//
+//@Configuration
+//@ConfigurationProperties(prefix = "spring.datasource")
+//public class HikariDatasourceConfig extends HikariConfig {
+//	
+//	@Value("${spring.datasource.username}")
+//	private String userName;
+//	
+//	@Value("${spring.datasource.password}")
+//	private String pwd;
+//
+//	@Bean
+//	@Primary
+//	public DataSource dataSource() throws SQLException {
+//		
+//		// use the credential configured in Vault to replace the spring config
+//		this.setUsername(this.userName);
+//		this.setPassword(this.pwd);
+//		
+//		return new HikariDataSource(this);
+//	}
+//
+//}
